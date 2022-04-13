736279f990dd5c3e8b67d839cba2b8f381f9b26b
root <EliteBoatengJohnson@gmail.com>
2022-04-13 15:16:34 -0700

    error fix

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,35 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * main - prints opcode of own main function
+ *
  * @argc: argument count
- * @argv: array of arguments
- * Return: 1 or 2 on fail, 0 on success
+ * @argv: arg value
+ * Return: int
  */
 int main(int argc, char *argv[])
 {
-	int bytes, i;
-	unsigned char *func_ptr;
+	int x, i;
+	unsigned char *p;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	bytes = atoi(argv[1]);
-	if (bytes < 0)
+	x = atoi(argv[1]);
+	if (x < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	func_ptr = (unsigned char *)main;
+	p = (unsigned char *)main;
 	i = 0;
-	if (bytes > 0)
+	if (x > 0)
 	{
-		while (i < (bytes - 1))
-			printf("%02hhx ", func_ptr[i++]);
-		printf("%hhx\n", func_ptr[i]);
+		while (i < (x - 1))
+			printf("%02hhx ", p[i++]);
+		printf("%hhx\n", p[i]);
 	}
 	return (0);
 }
